8a17381ef2dc4a96b5e84ea4f3fbb704cf39bc6b
nukosuke <39.nuko@gmail.com>
2016-06-11 12:18:44 +0900

    impled records service

--- src/services/records-service.ts
+++ src/services/records-service.ts
@@ -1,6 +1,54 @@
 import { HttpClient } from '../http-client';
+import { Order }      from '../string-literal';
+import { Work }       from './works-service';
+import { Episode }    from './episodes-service';
+
+export interface User {
+    id            : number
+    username      : string
+    name          : string
+    description   : string
+    url           : string
+    records_count : number
+    created_at    : Date
+}
+
+export interface Record {
+    id             : number
+    comment        : string
+    rating         : number
+    is_modified    : boolean
+    likes_count    : number
+    comments_count : number
+    created_at     : Date
+    user           : User
+    work           : Work
+    episode        : Episode
+}
+
+export interface RecordsRequestQuery {
+    fields            : string[]
+    filter_ids        : number[]
+    filter_episode_id : number
+    page              : number
+    per_page          : number
+    sort_id           : Order
+    sort_likes_count  : Order
+}
+
+export interface RecordsResponse {
+    records     : Record[]
+    total_count : number
+    next_page   : number
+    prev_page   : number
+}
 
 export class RecordsService {
-    constructor( client: HttpClient ) {
+    constructor( private client: HttpClient ) {
+    }
+
+    get(query?: RecordsRequestQuery): Promise<RecordsResponse> {
+        return this.client.get('https://api.annict.com/v1/records', query)
+        .then(response => response.json());
     }
 }
