f69a446b4b3390d9e204ddf35cb39b92e4e81d8a
nukosuke <39.nuko@gmail.com>
2016-06-11 09:03:49 +0900

    turn on works, episodes

--- src/annict.ts
+++ src/annict.ts
@@ -1,25 +1,26 @@
 import { HttpClient } from './http-client';
 import {
   AuthorizationService,
-  //WorksService,
-  //EpisodesService,
+  WorksService,
+  EpisodesService,
   //RecordsService,
   //MeService
 } from './services';
 
 export default class Annict {
-  public client : HttpClient
-  public oauth  : AuthorizationService
-  //public  works     : WorksService
-  //public  episodes  : EpisodesService
+  public client   : HttpClient
+  public oauth    : AuthorizationService
+  public works    : WorksService
+  public episodes : EpisodesService
   //public  records   : RecordsService
   //public  me        : MeService
 
   constructor() {
-      this.client    = new HttpClient();
-      this.oauth     = new AuthorizationService( this.client );
-      //this.works     = new WorksService        ( this.client );
-      //this.episodes  = new EpisodesService     ( this.client );
-      //this.me        = new MeService           ( this.client );
+      this.client   = new HttpClient();
+      this.oauth    = new AuthorizationService( this.client );
+      this.works    = new WorksService        ( this.client );
+      this.episodes = new EpisodesService     ( this.client );
+      //this.records  = new RecordsService      ( this.client );
+      //this.me       = new MeService           ( this.client );
   }
 }
