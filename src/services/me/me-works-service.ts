9ddb1457664a3c94a31898ff64794c597076f547
nukosuke <39.nuko@gmail.com>
2016-06-13 00:51:27 +0900

    split me-works into me-works-service

---
+++ src/services/me/me-works-service.ts
@@ -0,0 +1,26 @@
+import { HttpClient }    from '../../http-client';
+import { Order, Status } from '../../string-literal';
+import { WorksResponse } from '../works-service';
+
+export interface MeWorksGetRequestQuery {
+    fields              : string[]
+    filter_ids          : number[]
+    filter_season       : string[]
+    filter_title        : string
+    filter_status       : Status
+    page                : number
+    per_page            : number
+    sort_id             : Order
+    sort_season         : Order
+    sort_watchers_count : Order
+}
+
+export class MeWorksService {
+    constructor( private client: HttpClient ) {
+    }
+
+    get( query: MeWorksGetRequestQuery ): Promise<WorksResponse> {
+        return this.client.get('https://api.annict.com/v1/me/works', query)
+        .then(response => response.json());
+    }
+}
