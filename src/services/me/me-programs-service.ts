1127ea60cc5aff8be02caeef24b6cb8368a74137
nukosuke <39.nuko@gmail.com>
2016-06-13 02:48:40 +0900

    impled program service

--- src/services/me/me-programs-service.ts
+++ src/services/me/me-programs-service.ts
@@ -1,11 +1,45 @@
-import { HttpClient } from '../../http-client';
+import { HttpClient }    from '../../http-client';
+import { Order, BooleanString } from '../../string-literal';
+import { Work, WorkId }          from '../works-service';
+import { Episode }       from '../episodes-service';
+
+export type ChannelId = number
+export type ProgramId = number
+
+export interface Channel {
+    id   : ChannelId
+    name : string
+}
+
+export interface Program {
+    id             : ProgramId
+    started_at     : Date
+    is_rebroadcast : BooleanString
+    channel        : Channel
+    work           : Work
+    episode        : Episode
+}
 
 export interface MeProgramsGetRequestQuery {
-
+    fields               : string[]
+    filter_ids           : ProgramId[]
+    filter_channel_ids   : ChannelId[]
+    filter_work_ids      : WorkId[]
+    filter_started_at_gt : Date
+    filter_started_at_lt : Date
+    filter_unwatched     : BooleanString
+    filter_rebroadcast   : BooleanString
+    page                 : number
+    per_page             : number
+    sort_id              : Order
+    sort_started_at      : Order
 }
 
 export interface MeProgramsGetResponse {
-
+    programs    : Program[]
+    total_count : number
+    next_page   : number
+    prev_page   : number
 }
 
 export class MeProgramsService {
