34ad820f52a5fda2ebcedb97df3d642aedafc87d
nukosuke <39.nuko@gmail.com>
2016-06-13 00:50:40 +0900

    split me-status into me-statuses-service

---
+++ src/services/me/me-statuses-service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '../../http-client';
+import { Status }     from '../../string-literal';
+
+export interface MeStatusCreateRequestQuery {
+    work_id : number
+    kind    : Status
+}
+
+export class MeStatusesService {
+    constructor( private client: HttpClient ) {
+    }
+
+    create( query: MeStatusCreateRequestQuery ): Promise<IResponse> {
+        return this.client.post('https://api.annict.com/v1/me/statuses', query);
+    }
+}
