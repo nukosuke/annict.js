fadf8381fd7c49e01995787b6cdc0266dfe4366f
nukosuke <39.nuko@gmail.com>
2016-06-13 02:50:13 +0900

    activate me-programs

--- src/services/me-service.ts
+++ src/services/me-service.ts
@@ -2,18 +2,18 @@ import { HttpClient }          from '../http-client';
 import { MeStatusesService }   from './me/me-statuses-service';
 import { MeRecordsService }    from './me/me-records-service';
 import { MeWorksService }      from './me/me-works-service';
-//import { MeProgramsService }   from './me/me-programs-service';
+import { MeProgramsService }   from './me/me-programs-service';
 
 export class MeService {
     public Status  : MeStatusesService
     public Record  : MeRecordsService
     public Work    : MeWorksService
-    //public Program : MeProgramService
+    public Program : MeProgramsService
 
     constructor( private client: HttpClient ) {
         this.Status  = new MeStatusesService( client );
         this.Record  = new MeRecordsService ( client );
         this.Work    = new MeWorksService   ( client );
-        //this.Program = new MeProgramsService( client );
+        this.Program = new MeProgramsService( client );
     }
 }
