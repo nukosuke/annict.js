fdbe114360a4dcb25626f226a42236a3b687db99
nukosuke <39.nuko@gmail.com>
2016-06-16 19:47:35 +0900

    setup browser build

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/annict.ts',
+    output: {
+        filename: 'annict.min.js',
+        path: './browser',
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts']
+    },
+    module: {
+        loaders: [
+            { test: /\.ts$/, loader: 'ts' }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            VERSION: JSON.stringify(require('./package.json').version),
+            BROWSER: true,
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+}
