0bfc7424b7a4b11f6d083b9c2241a884b07acd56
nukosuke <nukosuke@shibuya.tech>
2016-09-25 21:51:51 +0900

    add typedoc comment

--- src/http-client.ts
+++ src/http-client.ts
@@ -3,23 +3,41 @@ import { Promise } from 'es6-promise';
 import * as qs     from 'qs';
 
 /**
- * wrapper class of fetch API
+ * fetch APIのラッパークラス
  */
 export class HttpClient {
+
+    /**
+     * HTTPヘッダのハッシュ
+     */
     private headers: {[index:string]: string} = {
         'Accept'       : 'application/json',
         'Content-Type' : 'application/json',
         'User-Agent'   : 'annict.js',
     };
 
+    /**
+     * ヘッダの値を取得する
+     * @param key ヘッダのフィールド名
+     */
     getHeader( key: string ): string {
         return this.headers[key];
     }
 
+    /**
+     * ヘッダの値を設定する
+     * @param key   ヘッダのフィールド名
+     * @param value ヘッダの値
+     */
     setHeader( key: string, value: string ): void {
         this.headers[key] = value;
     }
 
+    /**
+     * URLに対してGETリクエストを行う
+     * @param url   リクエスト先のURL
+     * @param query クエリのハッシュ
+     */
     get( url: string, query?: {[key:string]: any} ): Promise<IResponse> {
         if (query) {
             url += '?' + qs.stringify(query, { arrayFormat: 'repeat' });
@@ -30,6 +48,11 @@ export class HttpClient {
         });
     }
 
+    /**
+     * URLに対してPOSTリクエストを行う
+     * @param url   リクエスト先のURL
+     * @param body  リクエストbody
+     */
     post( url: string, body: any ): Promise<IResponse> {
         return fetch(url, {
             method : 'POST',
@@ -38,6 +61,11 @@ export class HttpClient {
         });
     }
 
+    /**
+     * URLに対してPATCHリクエストを行う
+     * @param url   リクエスト先のURL
+     * @param body  リクエストbody
+     */
     patch( url: string, body: any ): Promise<IResponse> {
         return fetch(url, {
             method : 'PATCH',
@@ -46,6 +74,10 @@ export class HttpClient {
         });
     }
 
+    /**
+     * URLに対してDELETEリクエストを行う
+     * @param url リクエスト先のURL
+     */
     delete( url: string ): Promise<IResponse> {
         return fetch(url, {
             method: 'DELETE',
