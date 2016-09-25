0c6bcafdb17020ecd8cfb6a24de5a5e944a13d3e
nukosuke <nukosuke@shibuya.tech>
2016-09-25 12:10:40 +0900

    fix typo

--- build_documents.sh
+++ build_documents.sh
@@ -5,5 +5,5 @@ git checkout --orphan gh-pages
 ls | grep -v '^docs$' | xargs rm -rf
 mv ./docs/* . && rmdir docs
 git add . && \
-git commit -m "build $(git descripe --abbrev=0 master) document\n$(date)" && \
+git commit -m "build $(git describe --abbrev=0 master) document\n$(date)" && \
 git push origin gh-pages
