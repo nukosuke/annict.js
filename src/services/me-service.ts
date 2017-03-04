f52f0c4cd8fc8186bf57940d6ba0d460e4b7ff3b
nukosuke <39.nuko@gmail.com>
2017-03-04 19:20:56 +0900

    issue #71, support /v1/me

--- src/services/me-service.ts
+++ src/services/me-service.ts
@@ -5,6 +5,35 @@ import { MeWorksService }      from './me/me-works-service';
 import { MeProgramsService }   from './me/me-programs-service';
 
 /**
+ * me fields of API response
+ */
+export interface Me {
+  id                   : number
+  username             : string
+  name                 : string
+  description          : string
+  url                  : string
+  avatar_url           : string
+  background_image_url : string
+  records_count        : number
+  created_at           : Date
+  email                : string
+  notifications_count  : number
+}
+
+/**
+ * query parameters for request
+ */
+export interface MeGetRequestQuery {
+  fields : string[]
+}
+
+/**
+ * response of me
+ */
+export type MeGetResponse = Me;
+
+/**
  * account service class
  */
 export class MeService {
@@ -21,5 +50,13 @@ export class MeService {
         this.Record  = new MeRecordsService ( client );
         this.Work    = new MeWorksService   ( client );
         this.Program = new MeProgramsService( client );
+    }
+
+    /**
+     * fetch item
+     * @param query query parameters
+     */
+    get(query: MeGetRequestQuery): Promise<Response> {
+      return this.client.get('https://api.annict.com/v1/me', query);
     }
 }
